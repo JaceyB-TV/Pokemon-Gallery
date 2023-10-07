5438de6d466e9bda70623919d92a9caed18ba767
JaceyB-TV <jaceyb.tv@gmail.com>
2023-10-07 03:22:01 +0100

    Removed unimplemented dao reference

--- web/fix.php
+++ web/fix.php
@@ -1,12 +1,17 @@
 <?php
 
-include '../secret/secret.php';
-include '../dao/type.php';
+include 'secret.php';
 
 $limit = isset( $_GET['limit'] ) ? intval( $_GET['limit'] ) : 10;
 $offset = isset( $_GET['offset'] ) ? intval( $_GET['offset'] ) : 0;
 
-$types = $type_dao->selectAll();
+$typeResult = $connection->query( "SELECT * FROM type" );
+$types = array();
+if ( $typeResult->num_rows > 0 ) {
+    while ( $row = $typeResult->fetch_assoc() ) {
+        $types[] = $row;
+    }
+}
 $typesByName = array();
 foreach ( $types as $type ) {
     $typesByName[strtolower( $type['name'] )] = $type['id'];
