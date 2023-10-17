7eb4c9be033cdfdb485b0c43daf7186052e34108
JaceyB-TV <139434535+JaceyB-TV@users.noreply.github.com>
2023-10-17 15:57:42 +0000

    Added

--- web/twitch/index.php
+++ web/twitch/index.php
@@ -6,3 +6,17 @@
         width="600"
         allowfullscreen>
 </iframe>
+
+<iframe
+        src="https://clips.twitch.tv/embed?clip=PeacefulVivaciousEagleLitFam&parent=jaceyb.co.uk"
+        height="800"
+        width="600"
+        allowfullscreen>
+</iframe>
+
+<iframe
+        src="https://clips.twitch.tv/embed?clip=PeacefulVivaciousEagleLitFam&parent=www.jaceyb.co.uk"
+        height="800"
+        width="600"
+        allowfullscreen>
+</iframe>
