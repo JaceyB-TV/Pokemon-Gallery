bfa7d1e9296653cdb056353728d07f33a2290c8a
JaceyB-TV <139434535+JaceyB-TV@users.noreply.github.com>
2023-10-17 16:00:04 +0000

    Updated twitch URL?

--- web/twitch/index.php
+++ web/twitch/index.php
@@ -1,21 +1,6 @@
 
-
 <iframe
-        src="https://www.twitch.tv/jaceyb/clip/PeacefulVivaciousEagleLitFam-g1xKLpbEfiHefZ_U/?parent=jaceyb.co.uk"
-        height="300"
-        width="400"
-        allowfullscreen>
-</iframe>
-
-<iframe
-        src="https://clips.twitch.tv/embed?clip=PeacefulVivaciousEagleLitFam&parent=jaceyb.co.uk"
-        height="300"
-        width="400"
-        allowfullscreen>
-</iframe>
-
-<iframe
-        src="https://clips.twitch.tv/embed?clip=PeacefulVivaciousEagleLitFam&parent=www.jaceyb.co.uk"
+        src="https://clips.twitch.tv/embed?clip=PeacefulVivaciousEagleLitFam-g1xKLpbEfiHefZ_U&parent=jaceyb.co.uk&parent=www.jaceyb.co.uk"
         height="300"
         width="400"
         allowfullscreen>
