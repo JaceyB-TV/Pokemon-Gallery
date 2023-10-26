3955f87b45cd99fd7924b69188bc722c35a7e66d
JaceyB-TV <139434535+JaceyB-TV@users.noreply.github.com>
2023-10-26 09:36:57 +0000

    Updated css and js reference

--- web/common/header.php
+++ web/common/header.php
@@ -6,9 +6,9 @@
     <title>Pokédex by Jacey</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-    <script src="../js/script.js<?php echo "?date=" . time() ?>"></script>
+    <script src="/js/script.js<?php echo "?date=" . time() ?>"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link type="text/css" rel="stylesheet" href="../css/style.css<?php echo "?date=" . time() ?>"></link>
+    <link type="text/css" rel="stylesheet" href="/css/style.css<?php echo "?date=" . time() ?>"></link>
 </head>
 <?php
 
